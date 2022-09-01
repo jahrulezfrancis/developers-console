5a3c486a18b9ae77d4626bf06af389d8dc08a6a5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-01 01:50:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './AppBar';
+import FixedBottomNavigation from './BottomNav';
+
+function App() {
+  return (
+    <div>
+     <NavigationBar />
+     <FixedBottomNavigation />
+    </div>
+  )
+}
+
+export default App;

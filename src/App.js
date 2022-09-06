fb95de049ad59ad290fb281b354868511cb96702
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-07 00:23:06 +0100

    updated the routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavigationBar from './AppBar';
 import FixedBottomNavigation from './BottomNav';
+import DemoCard from './Components/Interface/axiosDemo';
 import { Bookmark, Home, ProfilePage } from './Components/Interface/Routes';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path='home' element={<Home />}></Route>
       <Route path='bookmark' element={<Bookmark />}></Route>
       <Route path='profile' element={<ProfilePage />}></Route>
+      <Route path='demo' element={<DemoCard />}></Route>
      </Routes>
     </div>
   )
